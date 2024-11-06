b8d6c2a3521835f50c438e7f2761547874c7476a
enanaakaroku <furuwohuai@gmail.com>
2024-11-06 23:33:29 +0800

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import NeonLight from "@/components/NeonLight";
-
 export default function Home() {
   return (
     <div className="grid place-items-center h-screen">
-      <div className="w-[300px] h-[200px] rounded-md bg-gradient-to-br from-violet-500 from-30% via-indigo-500 via-60% to-blue-500 bg-[length:200%_100%] bg-clip-text">
-        <h1 className="text-4xl font-bold text-transparent text-shadow-white-md">Greeting</h1>
+      <div className="relative w-[300px] h-[200px] rounded-lg bg-gradient-to-br from-violet-500 from-30% via-indigo-500 via-60% to-blue-500 bg-[length:200%_100%]">
+        <div className="absolute inset-4 p-4 bg-black rounded-lg">
+          <h1 className="text-4xl font-bold">Greeting</h1>
+        </div>
       </div>
     </div>
   );
